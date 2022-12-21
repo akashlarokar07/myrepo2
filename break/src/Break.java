ca69d40f4fbe52cdf11e102a377ac8a3400a14f9
Akash Larokar <Akash Larokar@LAPTOP-RMA77M9O>
2022-12-21 11:05:13 +0530

    My second commit

---
+++ break/src/Break.java
@@ -0,0 +1,27 @@
+
+public class Break {
+
+	public static void main(String[] args)
+	{
+		// WAP to print numbers upto 5 by using "break" Statement.
+		
+		int x;
+		
+		for (x=0 ; x<=100 ; x++)
+		{
+			if(x==6)
+			break;
+
+			System.out.println(x);
+			
+//					if(x==3 || x==5 || x==7 )
+//			
+//				continue;
+			
+			
+			
+			
+		}
+	}
+
+}
